9db0780d2a448c18072e9f44e42532a3ad54aa34
LydieV <49896819+LydieV@users.noreply.github.com>
2019-12-12 22:06:20 +0100

    Modif page contact

--- wp-content/themes/my-theme/page.php
+++ wp-content/themes/my-theme/page.php
@@ -1,30 +1,20 @@
 <?php get_header(); ?>
-<h1>page.php</h1>
+
     <?php
         if ( have_posts() ) {
             while ( have_posts() ) {
-                
-    /**
-     * La méthode the_post() permet de charger le post courant
-     * Un post est un type de contenu, par exemple une actualité ou une page
-     **/
+   
                 the_post(); 
 
-    /**
-     * La méthode the_content() affiche le contenu du post en cours
-     * Il s'agit du contenu que vous avez renseigné dans le back-office
-     * Il existe d'autres méthodes, par exemple pour afficher le Titre du contenu, on peut utiliser la méthode the_title()
-     */
                 the_content();
                 
+                ?>
+                
+                <h2 class="wpcf7_title"> Contactez-nous </h2>
+                
+                <?php
                 echo do_shortcode('[contact-form-7 id="114" title="Contact form 1"]');
-        ?>
 
-<!-- VOTRE CODE HTML COMMENCE ICI -->
-
-
-
-    <?php
             }
         }
     ?>
